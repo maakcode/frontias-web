fee8068408ee63cd59edfd3f72204e7b44b9ee96
MaakCode <11623298+maakcode@users.noreply.github.com>
2024-12-01 23:55:12 +0900

    Updates

---
+++ _next/static/chunks/app/about/page-0a8bfdd9effed9eb.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[220],{6701:()=>{}},_=>{var e=e=>_(_.s=e);_.O(0,[185,541,358],()=>e(6701)),_N_E=_.O()}]);
---
+++ _next/static/chunks/pages/_app-1606cb75640caadb.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{3320:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(6003)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(3320),n(2297))),_N_E=_.O()}]);
